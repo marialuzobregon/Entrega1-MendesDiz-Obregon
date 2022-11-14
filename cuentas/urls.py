4874e4586bebbd301eb4e133c0c955ade72e4b3d
Felipe Mendes Diz <felipemendesdiz@gmail.com>
2022-11-13 21:49:16 -0300

    registro

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('inicio', views.inicio, name='inicio'),
+    path('registrar', views.registrar, name='registrar'),
     path('salir', LogoutView.as_view(template_name='cuentas/salir.html'), name='salir')
 ]
--- cuentas/views.py
+++ cuentas/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
 
 def inicio(request):
@@ -14,3 +14,15 @@ def inicio(request):
         formulario = AuthenticationForm()
     
     return render(request, 'cuentas/inicio.html', {'formulario': formulario})
+
+def registrar(request):
+    
+    if request.method == 'POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('index') 
+    else:
+        formulario = UserCreationForm()
+    
+    return render(request, 'cuentas/registrar.html', {'formulario': formulario})
